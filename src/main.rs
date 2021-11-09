72b02b5bd7d9490779d2861bb03c1752c299386d
krterberg <github@terberg.dev>
2021-11-09 11:27:27 +0100

    Implement list of civilizations and sampling logic

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    use rand::Rng;
+
+    let civs: Vec<&str> = vec!(
+        "Rus",
+        "Holy Roman Empire",
+        "Chinese",
+        "English",
+        "Delhi Sultanate",
+        "Mongols",
+        "Abbasid Dynasty",
+        "French"
+    );
+
+    let mut rng = rand::thread_rng();
+    let roll = rng.gen_range(0..civs.len());
+
+    println!("{}", civs[roll]);
+}

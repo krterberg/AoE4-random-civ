c55691554f2a56d9b9da7c8d8e8df17a26986ddd
krterberg <github@terberg.dev>
2021-12-18 17:07:23 +0100

    Set up arg parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use rand::Rng;
 use clap::{Arg, App};
 fn main() {
     
+    let matches = App::new("Random civ roller")
+    .version("0.2.0")
+    .about("Picks a random civ from the game Age of Empires 4 for you")
+    .arg(Arg::with_name("Include")
+             .short("i")
+             .long("include")
+             .takes_value(true)
+             .required(false)
+             .help("First letters of civs to include, as a string"))
+    .arg(Arg::with_name("Exclude")
+             .short("e")
+             .long("exclude")
+             .takes_value(true)
+             .required(false)
+             .help("First letters of civs to exclude, as a string"))
+    .get_matches();
 
     let civs: Vec<&str> = vec!(
         "Rus",
